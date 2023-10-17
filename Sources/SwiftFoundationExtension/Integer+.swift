678346ff3634daef681e66848382b0fa1b050635
Ji-Hwan KIM <nodealdev@gmail.com>
2023-10-17 13:14:47 +0900

    [Script push]

--- Sources/SwiftFoundationExtension/Integer+.swift
+++ Sources/SwiftFoundationExtension/Integer+.swift
@@ -9,7 +9,7 @@ import Foundation
 import Algorithms
 
 extension BinaryInteger {
-    public func insertComma() -> String {
+    public var commaSeperated: String {
         String(String(self).reversed().chunks(ofCount: 3).joined(separator: ",").reversed())
     }
 }
