964e059119de046bdb4ed0181eefe22e01ff9a18
Ji-Hwan KIM <nodealdev@gmail.com>
2023-10-17 17:13:12 +0900

    [Script push]

--- Sources/SwiftFoundationExtension/NSError+.swift
+++ Sources/SwiftFoundationExtension/NSError+.swift
@@ -11,6 +11,36 @@ extension NSError {
     public convenience init(domain: String, code: Int, underlying error: any Error) {
         self.init(domain: domain, code: code, userInfo: [NSUnderlyingErrorKey : error])
     }
+    
+    @available(iOS 14.5, *)
+    @available(macOS 11.3, *)
+    public func forEachUnderlying(action: (NSError) -> Void?) -> Void? {
+        if action(self) == nil {
+            return nil
+        } else {
+            for underlying in underlyingErrors {
+                if (underlying as NSError).forEachUnderlying(action: action) == nil {
+                    break
+                }
+            }
+        }
+        
+        return ()
+    }
+    
+    @available(iOS 14.5, *)
+    @available(macOS 11.3, *)
+    public func firstUnderlying(domain: String, code: Int) -> NSError? {
+        var found: NSError? = nil
+        forEachUnderlying { error in
+            if error.domain == domain && error.code == code {
+                found = error
+                return nil
+            }
+        }
+        
+        return found
+    }
 }
 
 
