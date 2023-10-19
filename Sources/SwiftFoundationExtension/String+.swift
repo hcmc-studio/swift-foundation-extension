fef84aa0c5211c1d2a85db5d79e8d8d81c2dac38
Ji-Hwan KIM <nodealdev@gmail.com>
2023-10-19 15:00:26 +0900

    [Script push]

---
+++ Sources/SwiftFoundationExtension/String+.swift
@@ -0,0 +1,19 @@
+//
+//  File.swift
+//  
+//
+//  Created by Ji-Hwan Kim on 10/19/23.
+//
+
+import Foundation
+
+extension String {
+    public static func *(_ lhs: String, _ rhs: Int) -> String {
+        var acc = ""
+        for _ in 0..<rhs {
+            acc += lhs
+        }
+        
+        return acc
+    }
+}
