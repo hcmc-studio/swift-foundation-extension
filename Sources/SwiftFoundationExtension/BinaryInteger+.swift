5e5c2ca3f93f51057361f2e692f37ec8d44ce09c
Ji-Hwan KIM <nodealdev@gmail.com>
2023-10-19 15:37:31 +0900

    [Script push]

--- Sources/SwiftFoundationExtension/BinaryInteger+.swift
+++ Sources/SwiftFoundationExtension/BinaryInteger+.swift
@@ -1,8 +1,8 @@
 //
-//  File.swift
-//  
+//  BinaryInteger.swift
 //
-//  Created by Ji-Hwan Kim on 10/17/23.
+//
+//  Created by Ji-Hwan Kim on 10/19/23.
 //
 
 import Foundation
@@ -11,5 +11,9 @@ import Algorithms
 extension BinaryInteger {
     public var commaSeperated: String {
         String(String(self).reversed().chunks(ofCount: 3).joined(separator: ",").reversed())
+    }
+    
+    public func ceilIfEven() -> Self {
+        self + (self & 1)
     }
 }
